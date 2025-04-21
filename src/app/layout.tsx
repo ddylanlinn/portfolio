2aa73d694f1f6d3207022fd28a5ad6c0b1ad9da2
Dylan Lin <dylan.lin@jkopay.com>
2025-04-21 21:31:53 +0800

    feat: integrate Google Analytics for improved tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/context/ThemeContext'
 import { LanguageProvider } from '@/context/LanguageContext'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,20 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
+			<head>
+				<Script
+					src="https://www.googletagmanager.com/gtag/js?id=G-B0764TX0CF"
+					strategy="afterInteractive"
+				/>
+				<Script id="google-analytics" strategy="afterInteractive">
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-B0764TX0CF');
+					`}
+				</Script>
+			</head>
 			<body className={inter.className}>
 				<ThemeProvider>
 					<LanguageProvider>{children}</LanguageProvider>
